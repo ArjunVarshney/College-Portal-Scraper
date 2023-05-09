9dd90ad1acfdd8193907e146fed25e9821ec4898
Arjun Varshney <varshneyarjun49@gmail.com>
2023-05-10 05:16:23 +0530

    smaple files added

--- CollegeUpdate.bat
+++ CollegeUpdate.bat
@@ -1,3 +1,4 @@
 echo off
 
+:: Give the full path to your index.js file
 node "E:\Scrape College website\index.js"
---
+++ sample_config.js
@@ -0,0 +1,8 @@
+const environment_variables = {
+  COLLEGE_USER_ID: "College Portal User Id",
+  COLLEGE_PASSWORD: "College portal password",
+  NOTION_TOKEN: "Notion Integration token",
+  DATABASE_ID: "Notion Database id",
+};
+
+export default environment_variables;
